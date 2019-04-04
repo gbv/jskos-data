e851e8a4c241e54d76f945b02e386632ee447d0c
Dennis Tobola <Dennis.tobola@gbv.de>
2019-04-04 13:31:32 +0200

    Make 'txt2csv.py' work

--- bs/txt2csv.py
+++ bs/txt2csv.py
@@ -4,6 +4,19 @@ import re
 import csv
 from namedentities import unicode_entities
 
+#Liest die top.csv aus und gibt sie als Dictionary wieder
+top = {}
+
+with open('top.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        notation = row[0]
+        label = row[1]
+        top[notation] = label
+        
+
+#Die Ausgangsdatei sys.txt wird mit Skript in eine CSV-Datei konvertiert
+#Sonderfälle werden bestimmt
 record = {}
 
 csvfile = open('sys.csv', 'w', newline = '') 
@@ -14,7 +27,15 @@ def process_record():
     global csvwriter
     if bool(record) == True:
         if "hie" in record and "syt" in record and "syn" in record:
-            row = (record["hie"], record["syt"], record["syn"])
+            notation = record["syt"]
+            label = record["syn"]
+            level = int(record["hie"]) + 1
+            base = notation[0:3]
+            if base in top:
+                csvwriter.writerow((0, base, label))                
+                top.pop(base)
+                            
+            row = (level, notation, label)
             csvwriter.writerow(row)
         record = {}       
 
