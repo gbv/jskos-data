a17efe71d5039f17d2149e9421afa3b08d10a2a2
Dennis Tobola <Dennis.tobola@gbv.de>
2019-05-28 12:57:24 +0200

    Improve bos-concepts

--- bos/txt2csv.py
+++ bos/txt2csv.py
@@ -9,6 +9,9 @@ csvfile = open('sys.csv', 'w', newline = '')
 csvwriter = csv.writer(csvfile)
 csvwriter.writerow(("level","notation","prefLabel"))
 
+# Bereits verarbeitete Notationen
+seen = set()
+
 # Gibt einen Datensatz als CSV aus
 record = {}
 def process_record():
@@ -16,16 +19,21 @@ def process_record():
     global csvwriter
     if bool(record) == True:
         if "hie" in record and "syt" in record and "syn" in record:
-            notation = record["syt"]
-            label = record["syn"]
-            level = int(record["hie"]) + 1
-            base = notation[0:3]
-            if base in top:
-                csvwriter.writerow((0, base, label))                
-                top.pop(base)
+            notation = record["syt"]            
+            # Bereits verarbeitete Notationen
+            if notation in seen: 
+                print("Repeated Notation:", notation)
+            elif not re.search('^[a-z]{3} 000$', notation): # z.B. "all 000"
+                seen.add(notation) 
+                label = record["syn"]
+                level = int(record["hie"])
+                base = notation[0:3]
+                if base in top:
+                    csvwriter.writerow((0, base, label))
+                    top.pop(base)
                             
-            row = (level, notation, label)
-            csvwriter.writerow(row)
+                row = (level, notation, label)
+                csvwriter.writerow(row)
         record = {}       
 
 # Liest top.csv in ein dictionary
