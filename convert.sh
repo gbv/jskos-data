9f9434f24ba8726e8518639bb2e90e15ac9d434f
Jakob Voss <voss@gbv.de>
2019-06-06 09:31:30 +0200

    Show failing builds in red

--- convert.sh
+++ convert.sh
@@ -5,9 +5,15 @@ errors=0
 while read line; do
   dir=$(dirname $line)
   echo
-  echo "## $dir"
+  echo -e "\033[1m## $dir\e[0m"
   make -C "$dir"
-  errors=$((errors + $?))
+  code=$?
+  if [ $? == 0 ]; then
+    echo -e "\e[00;32mok\e[0m"
+  else
+    echo -e "\e[01;31mfailed\e[0m"
+    errors=$((errors+1))
+  fi
 done < <(git ls-files | grep Makefile)
 
 exit $errors
