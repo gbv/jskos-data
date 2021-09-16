48434875a1beacd278c1ca094a1629566173f61a
Jakob Voss <voss@gbv.de>
2021-09-16 13:17:42 +0200

    Add BK dump script

---
+++ bk/dump.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# download vocabulary via breadth-first search
+
+echo "root"
+curl -s 'http://api.dante.gbv.de/voc/bk/top?properties=*' | jq .[] -c > concepts.ndjson
+jq -r '.narrower[].uri' concepts.ndjson > queue
+
+echo $(wc -l queue) concepts to go...
+while [ -s queue ]; do
+  echo > current.ndjson 
+  while read URI; do
+    echo "$URI"
+    curl -s "http://api.dante.gbv.de/data?uri=$URI&properties=*" | jq -c .[] >> current.ndjson
+  done < queue
+  cat current.ndjson > concepts.ndjson
+  jq -r '.narrower[]?.uri?' current.ndjson > queue
+done
+
+echo $(wc -l concepts.ndjson) concept retrieved
