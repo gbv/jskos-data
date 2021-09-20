ddf781eb0c907dfec09cbc49a7560d1eabe36ee0
Stefan Peters <stefandesu@exo.pm>
2021-09-20 08:06:34 +0300

    Fix BK dump script

--- bk/dump.sh
+++ bk/dump.sh
@@ -8,12 +8,12 @@ jq -r '.narrower[].uri' concepts.ndjson > queue
 
 echo $(wc -l queue) concepts to go...
 while [ -s queue ]; do
-  echo > current.ndjson 
+  echo > current.ndjson
   while read URI; do
     echo "$URI"
     curl -s "http://api.dante.gbv.de/data?uri=$URI&properties=*" | jq -c .[] >> current.ndjson
   done < queue
-  cat current.ndjson > concepts.ndjson
+  cat current.ndjson >> concepts.ndjson
   jq -r '.narrower[]?.uri?' current.ndjson > queue
 done
 
