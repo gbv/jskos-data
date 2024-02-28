58ad9d9c632ff1bd9c6b295d3613b42bbbce28de
Stefan Peters <Stefan.Peters@gbv.de>
2024-02-28 13:37:57 +0100

    Update Docker entrypoint.sh

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -17,18 +17,3 @@ mkdir .backup
 echo "Node.js $(node --version)"
 echo "npm $(npm --version)"
 deno --version
-
-# # Pull changes
-# git pull
-
-# if [ -e _site/.git-commit ] && [ "$(git rev-parse HEAD)" == "$(cat _site/.git-commit)" ]; then
-#   echo "Site rebuild skipped because there was no update."
-# else
-#   # Might need to update dependencies after each pull
-#   npm ci
-#   # Build the site
-#   npm run build -- --pathprefix="$PATHPREFIX" --url="$URL"
-#   # Remember the current commit
-#   git rev-parse HEAD > _site/.git-commit
-# fi
-
