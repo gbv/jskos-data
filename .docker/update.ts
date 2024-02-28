1853344ca84e041a8b1ab5f46e1c799549438632
Stefan Peters <Stefan.Peters@gbv.de>
2024-02-28 13:18:16 +0100

    Docker: Deal with changes in local repo in update.ts

--- .docker/update.ts
+++ .docker/update.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env -S deno run --allow-read --allow-run --allow-env --allow-sys --ext=ts --lock=/usr/src/app/deno.lock
 
 import { $, cd } from "npm:zx@7"
+import { exists } from "https://deno.land/std/fs/exists.ts"
+import { parse as parsePath } from "https://deno.land/std/path/mod.ts"
 
 console.log("##########", new Date(), "Updating jskos-data repository...", "##########")
 
@@ -12,10 +14,46 @@ const updatedFiles = (await $`git fetch --quiet && git diff --name-only @ @{u}`.
 
 // TODO: Building vocabularies can modify files inside the repo, so we either need to reset it or possibly stash the changes -> still causes issues if there's a conflict!
 
+// Get current changes and reset them if they are also in updatedFiles
+const currentChanges = (await $`git status -s --no-rename`.quiet()).stdout.split("\n").filter(Boolean).map((line: string) => {
+  const [, tag, file] = line.match(/(.{2}) (.*)/) || []
+  if (!tag || !file) {
+    console.error("Error parsing changed file in line:", line)
+    return null
+  }
+  const change:any = { file }
+  if (tag === "??") {
+    change.added = true
+  } else if (tag === " M") {
+    change.modified = true
+  } else {
+    change.other = true
+  }
+  return change
+}).filter(Boolean)
+
+for (const change of currentChanges) {
+  if (updatedFiles.includes(change.file)) {
+    if (change.added) {
+      console.log(`- Found added file ${change.file} that would be overridden by incoming pull. Moving to .backup...`)
+      const path = parsePath(change.file)
+      let file = path.base
+      // Possibly also rename file
+      let i = 0
+      while (await exists(`.backup/${file}`)) {
+        file = `${path.name}.${i}${path.ext}`
+        i += 1
+      }
+      await $`mv ${change.file} .backup/${file}`
+    } else {
+      console.log(`- Found modified file ${change.file}, resetting it...`)
+      await $`git checkout -- ${change.file}`
+    }
+  }
+}
+
 // Update repo
-await $`git stash push -u -m before-pull`
 await $`git pull`
-await $`git stash pop stash@\{"$( (git stash list | grep -w before-pull) | cut -d "{" -f2 | cut -d "}" -f1)"\}`
 console.log()
 
 if (updatedFiles.includes("package-lock.json")) {
