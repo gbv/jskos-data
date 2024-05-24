f786f219cd340dbf9b476d120e6faffbd0367eca
Monty <Monty.Bitto@gbv.de>
2024-05-24 10:25:04 +0200

    Added extensions

--- crm/jsonl2jskos.py
+++ crm/jsonl2jskos.py
@@ -9,8 +9,6 @@ broader_relationships = {}
 with open(input_file, 'r') as infile:
     for line in infile:
         record = json.loads(line)
-        #erstelle Dictionaries mit den Relationen und den Ids, die such aus Notation und dem Label zusammensetzen.
-        #replacedBy und superClass wird hier als broader gesehen.
         if record.get('type') == 'edge' and 'from' in record and 'to' in record:
             from_node = record['from']
             to_node = record['to']
@@ -20,39 +18,49 @@ with open(input_file, 'r') as infile:
         elif 'id' in record:
             nodes[record['id']] = record
 
-#erstellt was in broader steht
 def create_broader(nodes_list):
     broader_nodes = []
     for node in nodes_list:
-        notation = node.split(' ', 1)[0]
-        uri = f"http://www.cidoc-crm.org/cidoc-crm/{notation}"
+        notation = node.split('_', 1)[0]
+        uri = determine_uri(notation, record.get('properties', {}))
         broader_nodes.append({
             "uri": uri,
             "notation": [notation]
         })
     return broader_nodes
 
+def determine_uri(notation, properties):
+    base_uri = f"http://www.cidoc-crm.org/cidoc-crm/{notation}"
+    if 'extension' in properties:
+        extensions = properties['extension']
+        if "archaeo" in extensions:
+            return f"http://www.cidoc-crm.org/extensions/crmarchaeo/{notation}"
+        if "sci" in extensions:
+            return f"http://www.cidoc-crm.org/extensions/crmsci/{notation}"
+        if "LRMoo" in extensions:
+            return f"www.cidoc-crm.org/extensions/lrmoo/{notation}" 
+    return base_uri
 
 with open(output_file, 'w') as outfile:
     for node_id, record in nodes.items():
-        #Id wird in die Notation und das Label zerlegt
-        id_parts = record['id'].split(' ', 1)
+        id_parts = record['id'].split('_', 1)
         notation = id_parts[0]
         pref_label = id_parts[1] if len(id_parts) > 1 else ''
-        
-        #Eine Zeile wird erstellt
+            
+        uri = determine_uri(notation, record.get('properties', {}))
+            
         new_record = {
             'notation': [notation],
             'prefLabel': {"en": pref_label},
-            'uri': f"http://www.cidoc-crm.org/cidoc-crm/{notation}",
+            'uri': uri,
             'inScheme': [{"uri": "http://bartoc.org/en/node/1644"}]
         }
+            
         if notation == "E1":
             new_record["topConceptOf"] = [{"uri": "http://bartoc.org/en/node/1644"}]
 
         if node_id in broader_relationships:
             new_record['broader'] = create_broader(broader_relationships[node_id])
-        
-
+            
         json.dump(new_record, outfile)
         outfile.write('\n')
