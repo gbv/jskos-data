7ec9da4e090f940e14b32ac6cfebcd18afa15d3d
Jakob Voss <voss@gbv.de>
2025-01-22 11:22:06 +0100

    Finish FBL

--- fbl/excel2csv.pl
+++ fbl/excel2csv.pl
@@ -1,9 +1,26 @@
 #!/usr/bin/env perl
 use v5.32;
 use Spreadsheet::XLSX;
+use Text::CSV;
 
 my $source = shift @ARGV or die "usage: $0 <source file>\n";
-my $excel  = Spreadsheet::XLSX->new->parse($source)
+my $excel  = Spreadsheet::XLSX->new($source)
   or die "Failed to open $source: $!";
 
+say 'level,notation,prefLabel@de,scopeNote@de';
+
 my $sheet = $excel->{Worksheet}[0];
+$sheet->{MaxRow} ||= $sheet->{MinRow};
+for my $row ( $sheet->{MinRow} .. $sheet->{MaxRow} ) {
+    $row = $sheet->{Cells}[$row];
+    my @c = map { $_ ? $_->{Val} : undef } map { $row->[$_] } 0 .. 6;
+
+    my ($level)  = grep { $c[$_] } 0 .. 5;
+    my $notation = $c[$level];
+    my $label    = $c[6];
+    my $note     = $c[7];
+
+    say join ",",
+      map { $_ =~ s/"/'/g; $_ =~ s/^\s+|\s+$//g; "\"$_\""; }
+      ( $level, $notation, $label, $note );
+}
